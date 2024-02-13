ba583f64856fc657f33b8cd1b6565f9258b6408d
Ariela Israel <arielaisrael12@gmail.com>
2024-02-13 00:17:49 -0500

    font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				ysa: ['Ysabeau Infant', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
